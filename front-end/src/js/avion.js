f9aa3c6e55431d5a8e6962941904a4dede586a42
maxi <maximiliano.mingarelli@hotmail.com>
2021-08-20 21:28:13 -0300

    feat agregar logica reserva

--- front-end/src/js/avion.js
+++ front-end/src/js/avion.js
@@ -1,39 +1,65 @@
-const maxSeats = 12;
+/**
+ * En este archivo debes agregar tus funciones para realizar los ejecricios
+ * del Bootcamp.
+ * =========
+ * Recuerda:
+ * =========
+ * Siempre que tengas dudas, primero investiga e intenta resolverlo solo,
+ * pero si te encuentras bloqueado, no dudes en pedir ayuda.
+ * 
+ */
+ let asientos = [
+  //[false,false,false],
+  //[false,false,false],
+  // [false,false,false]
 
-let countReservados = 0;
-let reservas = [
-  [false,false,false],
-  [false,false,false],
-  [false,false,false],
-  [false,false,false],
-];
-
-function nextAvailable(){
-  let asiento = "no se encuentra ningún asiento libre";
-
-  let rows = reservas.length;
-  outer_loop:
-  for(let i=0;i<rows;i++){
-   let columns = reservas[i].length;
-   for(let j=0;j<columns;j++){
-
-     if(reservas[i][j]==false){
-       countReservados=countReservados+1;
-       reservas[i][j] = true;
-       asiento = 'reservado el asiento de la fila: ' + i + ' columna: ' + j;
-       break outer_loop;
-     }
-
-   }
+  [false, false, false],
+  [true, true, true],
+  [true, true, true],
+  [true, true, true]
+];//modificar AVAILABLE CULEAO
+let countAvailable = () => {
+  let contador = 0;
+  for (let i = 0; i < asientos.length; i++) {
+      let fila = asientos[i];
+      for (let j = 0; j < fila.length; j++) {
+          let asiento = fila[j];
+          if (asiento === true) {
+              contador = contador + 1;
+          }
+      }
   }
-
-  return asiento;
+  return contador;
 }
 
-function countAvailable(){
-  return maxSeats-countReservados;
+let isFullOfCapacity = () => {
+  if (countAvailable() === 0) {
+      return true;
+  } else {
+      return false;
+  }
 }
+let nextAvailable = () => {
+  if (isFullOfCapacity() === true) {
+      return 'no existe asiento disponible bro'
+  } else {
+      for (let i = 0; i < asientos.length; i++) {
+          let fila = asientos[i];
+          for (let j = 0; j < fila.length; j++) {
+              let asiento = fila[j];
+              if (asiento === true) {
+                  let filaSeleccionada = document.getElementsByClassName('row')[i];
+                  let asientoSeleccionado = filaSeleccionada.getElementsByClassName('col-auto')[j];
+                  asientoSeleccionado.className = 'col-auto mx-2 fila__asiento--ocupado';
+                  asientos[i][j] = false;
+                  return 'reservado el asiento de la fila: ' + (i + 1) + ' columna: ' + (j + 1);
+              }
+          }
+      }
+  }
+}
+console.log(countAvailable())
 
-function isFullOfCapacity(){
-  return maxSeats==countReservados;
-}
+console.log(isFullOfCapacity())
+
+console.log(nextAvailable())
