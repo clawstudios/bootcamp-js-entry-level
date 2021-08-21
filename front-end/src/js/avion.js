52bd58e534a2bf2cac2541b41d6178c923105036
maxi <maximiliano.mingarelli@hotmail.com>
2021-08-21 18:27:39 -0300

    feat agregado warn y disabled

--- front-end/src/js/avion.js
+++ front-end/src/js/avion.js
@@ -8,58 +8,57 @@
  * pero si te encuentras bloqueado, no dudes en pedir ayuda.
  * 
  */
- let asientos = [
-  //[false,false,false],
-  //[false,false,false],
-  // [false,false,false]
+let btnPedir = document.querySelector('#btnPedir');
+let asientos = [
+    //[false,false,false],
+    //[false,false,false],
+    // [false,false,false]
 
-  [true, true, true],
-  [true, true, true],
-  [true, true, true],
-  [true, true, true]
-];//modificar AVAILABLE CULEAO
+    [true, true, true],
+    [true, true, true],
+    [true, true, true],
+    [true, true, true]
+];
 let countAvailable = () => {
-  let contador = 0;
-  for (let i = 0; i < asientos.length; i++) {
-      let fila = asientos[i];
-      for (let j = 0; j < fila.length; j++) {
-          let asiento = fila[j];
-          if (asiento === true) {
-              contador = contador + 1;
-          }
-      }
-  }
-  return contador;
+    let contador = 0;
+    for (let i = 0; i < asientos.length; i++) {
+        let fila = asientos[i];
+        for (let j = 0; j < fila.length; j++) {
+            let asiento = fila[j];
+            if (asiento === true) {
+                contador = contador + 1;
+            }
+        }
+    }
+    return contador;
 }
 
 let isFullOfCapacity = () => {
-  if (countAvailable() === 0) {
-      return true;
-  } else {
-      return false;
-  }
+    if (countAvailable() === 0) {
+        window.alert('no hay mas asientos ');
+        btnPedir.disabled = true;
+        return true;
+    } else {
+        return false;
+    }
 }
 let nextAvailable = () => {
-  if (isFullOfCapacity() === true) {
-      return 'no existe asiento disponible bro'
-  } else {
-      for (let i = 0; i < asientos.length; i++) {
-          let fila = asientos[i];
-          for (let j = 0; j < fila.length; j++) {
-              let asiento = fila[j];
-              if (asiento === true) {
-                  let filaSeleccionada = document.getElementsByClassName('row')[i];
-                  let asientoSeleccionado = filaSeleccionada.getElementsByClassName('col-auto')[j];
-                  asientoSeleccionado.className = 'col-auto mx-2 fila__asiento--ocupado';
-                  asientos[i][j] = false;
-                  return 'reservado el asiento de la fila: ' + (i + 1) + ' columna: ' + (j + 1);
-              }
-          }
-      }
-  }
+    if (isFullOfCapacity() === true) {
+        return 'no existe asiento disponible bro'
+    } else {
+        for (let i = 0; i < asientos.length; i++) {
+            let fila = asientos[i];
+            for (let j = 0; j < fila.length; j++) {
+                let asiento = fila[j];
+                if (asiento === true) {
+                    let filaSeleccionada = document.getElementsByClassName('row')[i];
+                    let asientoSeleccionado = filaSeleccionada.getElementsByClassName('col-auto')[j];
+                    asientoSeleccionado.className = 'col-auto mx-2 fila__asiento--ocupado';
+                    asientos[i][j] = false;
+                    return 'reservado el asiento de la fila: ' + (i + 1) + ' columna: ' + (j + 1);
+                }
+            }
+        }
+    }
 }
-console.log(countAvailable())
-
-console.log(isFullOfCapacity())
-
-console.log(nextAvailable())
+console.log(document.getElementsByClassName('btn'));
