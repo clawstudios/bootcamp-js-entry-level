77f211f1ad37c6fc862a9a8529e3e41f1dffcd6e
Pottert78 <88218365+Potter78@users.noreply.github.com>
2021-08-23 15:50:28 -0300

    copiado de archivo

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -8,3 +8,41 @@
  * pero si te encuentras bloqueado, no dudes en pedir ayuda.
  *
  */
+ const maxSeats = 12;
+
+ let countReservados = 0;
+ let reservas = [
+   [true,true,true],
+   [true,true,true],
+   [true,true,true],
+   [true,true,true],
+ ];
+ 
+ function nextAvailable(){
+   let asiento = "no se encuentra ningún asiento libre";
+ 
+   let rows = reservas.length;
+   outer_loop:
+   for(let i=0;i<rows;i++){
+    let columns = reservas[i].length;
+    for(let j=0;j<columns;j++){
+ 
+      if(reservas[i][j]==true){
+        countReservados=countReservados+1;
+        reservas[i][j] = false;
+        asiento = 'reservado el asiento de la fila: ' + i + ' columna: ' + j;
+        break outer_loop;
+      }
+ 
+    }
+   }
+ 
+   return asiento;
+ }
+ 
+ function countAvailable(){
+    return maxSeats-countReservados;
+ }
+ 
+ function isFullOfCapacity(){
+   return maxSeats==countReservados;
