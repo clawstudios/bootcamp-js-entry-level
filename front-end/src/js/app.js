f58ec6db3fdd48e7ffaa263cbcd8d4f79baa695f
FdeConde01 <fedeguz17.fgc@gmail.com>
2021-08-24 17:20:53 -0300

    Update app.js

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -7,4 +7,48 @@
  * Siempre que tengas dudas, primero investiga e intenta resolverlo solo,
  * pero si te encuentras bloqueado, no dudes en pedir ayuda.
  *
- */
+ */ 
+ let reservados = 0
+ let asientos = [
+     [true, true,true]
+     [true, true,true]
+     [true, true,true]
+     [true, true,true]
+ ];
+ 
+ let cantidad = asientos.length;
+ 
+ function nextAvailable(){
+ 
+     let estadoAsiento = "No se encuentran asientos disponibles"
+ 
+     outer_loop:
+     for(let i = 0; i < asientos; i++){
+         let columnas = asientos[i].length
+         for(let j = 0; j < columnas ; j++){
+ 
+             if(asientos[i][j]==true){
+                 reservados = reservados + 1
+                 asientos[i][j] = false;
+                 estadoAsiento = 'Reservado el asiento de la fila: ' + i + 'columna: ' + j;
+                 break outer_loop
+             }
+ 
+         }
+     }
+ }
+ 
+ return estadoAsiento
+ 
+ function countAvailable(){
+     return "Hay " + reservados + " asientos reservados"
+     return "Hay " + (cantidad - reservados) + " asientos libres" 
+ }
+ 
+ function fullOfCapacity(){
+     if(asientos == reservados){
+         let Alerta = "No hay asientos disponibles"
+         alert(Alerta);
+         throw new Error (Alerta);
+     }
+ }
