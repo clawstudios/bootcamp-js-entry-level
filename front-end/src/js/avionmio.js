9b8723df72b06bdb132d4fd3f6a51097a3bdaa33
Pottert78 <88218365+Potter78@users.noreply.github.com>
2021-08-24 23:53:33 -0300

    copia de app

---
+++ front-end/src/js/avionmio.js
@@ -0,0 +1,46 @@
+onst total_de_asientos= 12;
+
+function countAvailable(){
+// true = asiento vacio
+
+  let asiento_vacio= [
+    [true,true,true],
+    [true,true,true],
+    [true,true,true],
+    [true,true,true],
+   ];
+
+    let fila = matriz.lenght; i=0;
+    for (i; i < fila; i++){
+      let columna = matriz[i].lenght; j=0;
+        for (j; j< columna; j++){
+    
+          if( [i][j] == true){
+            suma (asiento_vacio)
+             }else{ 
+              null}
+          }   
+        }
+      }
+    }
+
+    return asiento_vacio;
+  }
+
+
+function isFullOfCapacity(){ 
+  if (total_de_asientos - asientos_vacios === 0);{
+      document.write("El avion esta completo");
+  }
+  
+}
+
+function nextAvailable(){
+  let asiento = "ocupar reservado"
+  if (i; i = false and j; j=false);
+    window.alert("Este asiento esta oupado, seleccione el siguiente")
+  }{else
+    asiento = "reservado el asiento de la fila: " + i + "columna: " + j;}
+
+  return asiento;
+}
