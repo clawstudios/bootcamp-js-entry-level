787d3376f789847e920c1ec337d8b9e1327d6a62
Emi <Emi@MacBook-Pro-de-Maria.local>
2021-08-30 23:14:54 -0300

    correcion avion

--- front-end/src/js/avionmio.js
+++ front-end/src/js/avionmio.js
@@ -1,7 +1,5 @@
-const total_de_asientos = 12;
-
 let asiento_vacio= [
-  [true,true,true],
+  [false,true,true],
   [true,false,true],
   [true,true,true],
   [true,true,true],
@@ -12,7 +10,7 @@ function countAvailable(){
   for (let i = 0; i< asiento_vacio.length;i++){
     let fila = asiento_vacio[i];
     for (let j = 0; j< fila.length; j++){
-      let columna = fila [i];
+      let asiento_vacio = fila [i];
       if (asiento_vacio === true){
         asiento = asiento ++
       }
@@ -22,33 +20,31 @@ function countAvailable(){
 }
 
 function isFullOfCapacity(){
-  let ocupados = (total_de_asientos- (countAvailable));
-  if (total_de_asientos - ocupados == 0){
-    window.alert ("completo");
-    return false}
-  else{
-    return true
+  if (countAvailable() == 0){
+      window.alert ("completo");
+      return false;
+    }else{
+      return true;
   }
 }
 
 function nextAvailable(){
     if (isFullOfCapacity() === true){
       return "no hay aseinto disponible"
-    }else{
-      for (let i = 0; i< asiento_vacio.length;i++){
-        let fila = asiento_vacio[i];
-          for (let j = 0; j< fila.length; j++){
-            let columna = fila [i];
-            let butaca = [i][j];
-              if (butaca === true){
-                let filaseleccionada = document.getElementsByClassName('row')[i];
-                let asientoseleccionado = filaseleccionada.getElementsByClassName('col - auto')[j];
-                asientoseleccionado.className='col-auto mx-2 fila_asiento--ocupado';
-                butaca [i][j] = false;
-                return 'reservado el asiento de la fila:'+ (i + 1) + 'columna:' + (j + 1);     
+    } else{
+        for (let i = 0; i < asiento_vacio.length;i++){
+            let fila = asiento_vacio[i];
+            for (let j = 0; j< fila.length; j++){
+              let asiento_vacio = fila [i];
+              if (asiento_vacio === true){
+                  let filaseleccionada = document.getElementsByClassName('row')[i];
+                  let asientoseleccionado = filaseleccionada.getElementsByClassName('col - auto')[j];
+                  asientoseleccionado.className ='col-auto mx-2 fila_asiento--ocupado';
+                  asiento_vacio [i][j] = false;
+                  return 'reservado el asiento de la fila:'+ (i + 1) + 'columna:' + (j + 1);     
               }
-          }
-      } 
+            }
+        } 
     } 
 }
 
