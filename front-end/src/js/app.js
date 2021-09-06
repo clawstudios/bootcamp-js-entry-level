fbaa9f31c27831740527a8e4752b9b6aa8226857
FdeConde01 <fedeguz17.fgc@gmail.com>
2021-09-06 17:58:27 -0300

    Ejercicio diabetes actualizado

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -8,3 +8,46 @@
  * pero si te encuentras bloqueado, no dudes en pedir ayuda.
  *
  */ 
+ const getGames = async () => {
+    let respuestaAPI = await fetch(
+      "https://free-to-play-games-database.p.rapidapi.com/api/games",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+          "x-rapidapi-key": "ecec6fb34amsh3f55e059d5eca57p14aee7jsn256e8e8bd65b",
+        },
+      }
+    );
+    let body = await respuestaAPI.json();
+    return body;
+  };
+  const getJuegos = async () => {
+    let Juegos = await getGames();
+    for (let i = 0; i < Juegos.length; i++) {
+      let max = juegos[i];
+    }
+  };
+  const listGames = async () => {
+    let games = await getGames();
+    for (let i = 0; i < games.length; i++) {
+      let item = document.createElement("div");
+      item.className = "col-md-6 col-lg-4 mb-5";
+      item.innerHTML = `<div class="game-list-item mx-auto">
+              <div class="game-header">
+                  <h5>${games[i].title}</h5> 
+              </div>
+              <img class="img-fluid" src="${games[i].thumbnail}" alt="..." />
+              <p class="game-item-description">${games[i].short_description}</p>
+              <ul class="inline-list">
+                  <li><span class="badge badge-primary bg-badge">${games[i].genre}</span></li>
+                  <li><span class="badge badge-primary bg-badge">${games[i].platform}</span></li>
+                  <li><span class="badge badge-primary bg-badge">${games[i].publisher}</span></li>
+                  <li><span class="badge badge-primary bg-badge">${games[i].developer}</span></li>
+              </ul>
+              <a class="nav-link check-it-out-btn" href="${games[i].game_url}">Check it out!</a>
+          </div>`;
+      document.getElementById("gamer-grid").appendChild(item);
+    }
+  };
+  listGames();
