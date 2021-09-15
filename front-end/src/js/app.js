0d8ebddcd3d0454b139dab87bf867842392b8fe3
FdeConde01 <fedeguz17.fgc@gmail.com>
2021-09-15 15:57:41 -0300

    .

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -40,20 +40,20 @@ const fondoClean = () => {
     );
     let body = await respuestaAPI.json();
     hideLoader()
-    //fondoClean()
     return body;
     
   };
-  const getJuegos = async () => {
-    let Juegos = await getGames();
-    for (let i = 0; i < Juegos.length; i++) {
-      let max = juegos[i];
-    }
-  };
-  const listGames = async () => {
-    
+  const listGames = async (page) => {
     let games = await getGames();
-    for (let i = 0; i < games.length; i++) {
+    let gamesConteo = games.length;
+    let gamesXPage = 9;
+    let gamesPage = gamesXPage * page;
+    let maxPages = Math.ceil(games.length / gamesXPage);
+    console.log(maxPages)
+
+    document.getElementById('gamer-grid').innerHTML="";
+    
+    for (let i = (page - 1) * gamesXPage; i < gamesPage ; i++) {
       let item = document.createElement("div");
       item.className = "col-md-6 col-lg-4 mb-5";
       item.innerHTML = `<div class="game-list-item mx-auto">
@@ -73,4 +73,74 @@ const fondoClean = () => {
       document.getElementById("gamer-grid").appendChild(item);
     }
   };
-  listGames();
+  
+ const generatePagination = (maxPages, gamesXPage) => { 
+    for(let i = 0; i < maxPages; i++){
+      let item = document.createElement('li')
+      item.className = 'page-item';
+      item.innerHTML = `<a href="#">${i + 1}</a>`;
+      document.getElementById("pagination-id").appendChild(item);
+    }   
+  };
+//Buscador
+  const resultado = document.querySelector('#resultado')
+  const formulario = document.querySelector('#inputSearch');
+  const boton = document.querySelector('#boton');
+  const filtrar = () => {
+      resultado.innerHTML = ''
+
+      const texto = formulario.value.toLowerCase()
+      
+      for(let producto of productos){
+          let nombre = getGames.tittle.toLowerCase();
+          if(nombre.indexOf(texto) !== -1){
+              resultado.innerHTML += `<li>${getGames.tittle}</li>`
+          }
+      }
+      if(resultado.innerHTML === ''){
+          resultado.innerHTML += `<li>Juego no encontrado</li>`
+      }
+  }
+  boton.addEventListener('click', filtrar)
+  formulario.addEventListener('keyup', filtrar)
+  filtrar();
+ 
+//Buscador
+//Variables
+/* 
+bars_search = document.getElementById("ctn-bars-search");
+cover_ctn_search = document.getElementById("cover_ctn_search");
+inputSearch = document.getElementById("inputSearch");
+box_search = document.getElementById("box_search");
+
+//Filtrado de busqueda
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
+const buscador_interno = () => {
+
+  filter = inputSearch.value.toUpperCase();
+  li = box_search.getElementsByTagName("li");
+  
+//Recorriendo elementos al filtrar
+  for (i = 0; i < li.length; i++){
+    a = li[i].getElementsByTagName("a")[0];
+    textValue = a.textContent || a.innerText;
+    if(textValue.toUpperCase().indexOf(filter) > -1){
+      li[i].style.display = "";
+      box_search.style.display = "block";
+
+      if (inputSearch.value ===  ""){
+        box_search.style.display = "none"
+      }
+      
+    }else{
+      li[i].style.display = "none";
+    }
+  }
+
+
+};
+*/
+
+listGames(1).then(()=>{
+  generatePagination(20);
+})
